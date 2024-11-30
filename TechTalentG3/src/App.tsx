2f2cff967ad3a02e90dbd5c4b99ba2a81519a3e5
Khady Yade <khady.yade@estudiants.urv.cat>
2024-11-30 13:51:35 +0100

    Khady: Pagina de modo Cine y funcion añadir eventos en proceso

--- TechTalentG3/src/App.tsx
+++ TechTalentG3/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import EventList from "../componentes/EventList";
+import AddEventForm from "../componentes/AddEvent";
+import { Event } from "../componentes/Event"; // Importa la interfaz
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [events, setEvents] = useState<Event[]>([]); // Estado de los eventos
+  const [showForm, setShowForm] = useState(false);
+
+  const handleAddEvent = (newEvent: Event) => {
+    setEvents((prevEvents) => [...prevEvents, newEvent]); // Agrega el nuevo evento
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Modo Cine</h1>
+      <button className="add-event-btn" onClick={() => setShowForm(true)}>
+        Añadir Evento
+      </button>
+      {showForm && (
+        <AddEventForm
+          onAddEvent={handleAddEvent}
+          onClose={() => setShowForm(false)}
+        />
+      )}
+      <EventList events={events} />
+    </div>
+  );
+};
 
-export default App
+export default App;
