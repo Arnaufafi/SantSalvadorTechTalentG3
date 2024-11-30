4ba8584fd8bd362a8240bb687bf58bee6a0c532b
SMO Aula <smoaula@PAJT9543.ajtarragona.es>
2024-11-30 11:42:40 +0100

    Header

--- TechTalentG3/src/App.tsx
+++ TechTalentG3/src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import MapComponent from './components/MapComponent';
+import Header from './componentes/HeaderInteractivo'
 
 function App() {
   return ( 
     <div className="mapComponent">
         <h1>My Map View</h1>
+        <Header/>
         <MapComponent />
     </div>
   );
